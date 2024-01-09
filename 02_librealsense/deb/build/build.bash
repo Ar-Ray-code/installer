623dc00bcdc9e1c5821ed01f9aa0144b21eebdc7
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2024-01-09 22:13:31 +0900

    Update build.bash

--- 02_librealsense/deb/build/build.bash
+++ 02_librealsense/deb/build/build.bash
@@ -6,6 +6,6 @@ cd $SCRIPT_DIR
 git clone https://github.com/IntelRealSense/librealsense.git -b $TAG
 cd librealsense
 mkdir -p build && cd build
-cmake -DBUILD_EXAMPLES=false -DUILD_GRAPHICAL_EXAMPLES=false -DBUILD_WITH_OPENMP=true -DBUILD_PYTHON_BINDINGS=false -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=Release ..
+cmake -DBUILD_EXAMPLES=false -DBUILD_GRAPHICAL_EXAMPLES=false -DBUILD_WITH_OPENMP=true -DBUILD_PYTHON_BINDINGS=false -DFORCE_RSUSB_BACKEND=true -DCMAKE_BUILD_TYPE=Release ..
 make -j$(nproc)
 make install
